241e7c148f0b86c15ad07df52bb67588c9bc7db5
Rutul Shah <rutulshah17@gmail.com>
2021-07-18 05:41:21 -0400

    moving every component 1 level up

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,83 @@
 import React from 'react';
-import Slider from './components/Slider/Slider.components';
+
+import { Container, Box, Typography } from '@material-ui/core';
+import { ToggleButtonGroup, ToggleButton } from '@material-ui/lab';
+
+import { fetchCats, fetchSharks, fetchCatsAndSharks } from './api/app.api';
+
+import Cats from './components/Cats/Cats.components';
+import Sharks from './components/Sharks/Sharks.components';
+import CatsAndSharks from './components/CatsAndSharks/CatsAndSharks.components';
+import Loader from './components/Loader/Loader.component';
+
+import './App.css';
 
 class App extends React.Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            catsButton: false,
+            sharksButton: false,
+            cats:[],
+            sharks:[],
+            catsAndSharks:[]
+        }
+    }
+
+    componentDidMount = async() => {
+        const fetchedCats = await fetchCats();
+        const fetchedSharks = await fetchSharks();
+        const fetchedCatsAndSharks = await fetchCatsAndSharks();
+
+        this.setState({ catsButton: !this.state.catsButton, cats: fetchedCats, sharks: fetchedSharks, catsAndSharks: fetchedCatsAndSharks });
+    }
+
+    handleCats = async() => {
+        this.setState({ catsButton: !this.state.catsButton, cats: this.state.cats });
+    }
+
+    handleSharks = async() => {
+        this.setState({ sharksButton: !this.state.sharksButton, sharks: this.state.sharks });
+    }
+
     render() {
+    
+        const { catsButton, cats, sharksButton, sharks, catsAndSharks } = this.state;
+
         return (
-            <div>
-                <Slider />
-            </div>
+
+                <Container disableGutters={true}>
+                    <Box>
+                        <ToggleButtonGroup className='app-toggle-button' >
+                            <ToggleButton selected={this.state.catsButton} onClick={this.handleCats} value='Cats'> 
+                                <Typography color='primary' variant='h5'> Cats </Typography> 
+                            </ToggleButton>
+                            
+                            <ToggleButton selected={this.state.sharksButton} onClick={this.handleSharks} value='Sharks'> 
+                                <Typography color='primary' variant='h5'> Sharks </Typography> 
+                            </ToggleButton>
+                        </ToggleButtonGroup>
+                    </Box>
+                    
+                    {
+                        //show loader if waiting on data from API
+                        (!cats || !sharks) 
+                        ? <Loader />
+                        : 
+                        //show Cats And Sharks - both, if catsButton and sharksButton value matches
+                            (catsButton === sharksButton)
+                            ? <CatsAndSharks catsAndSharks={catsAndSharks}/>
+                            : ( (catsButton)
+                                ? <Cats cats={cats} />
+                                : <Sharks sharks={sharks} />
+                                )
+                    }
+                
+                </Container>
         )
     }
-}
+};
 
 export default App;

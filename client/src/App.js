016f6d30d5c486b3b8d9e82757aa43cda32c3c62
Rutul Shah <rutulshah17@gmail.com>
2021-07-18 05:11:44 -0400

    minor changes, exporting data provided, removing commented code

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Slider from './components/Slider/Slider.components';
-//import { fetchCats, fetchSharks, fetchBoth } from '../src/api/app.api';
 
 class App extends React.Component {
 
